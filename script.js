d93b8ad3c67f8233e870486dac125063b35ea93c
Faik-man <faik199417@gmail.com>
2024-07-12 19:47:56 +0300

    Первый прототип перевода js скрипта на архитектуру MVC.

--- script.js
+++ script.js
@@ -8,6 +8,317 @@ function supports_html5_storage()
 	return window.localStorage != null && window['localStorage'] != null;
 }
 
+class Model {
+    #arrayOfSelectedAnswers;
+    #indexTest;
+
+    constructor() {
+        this.#arrayOfSelectedAnswers = loadAnswersFromStorage();
+        this.#indexTest = loadIndexTestFromStorage();
+
+        this.testArray = test_array;
+    }
+
+    bindModelDataChanged(callback) {
+        this.onDataChanged = callback;
+    }
+
+    getQuestion() {
+        return (this.#indexTest === -1
+            ? ''
+            : (this.#indexTest + 1) + ') ' + this.testArray[this.#indexTest].question);
+    }
+
+    getNumQuestions() {
+        return this.testArray.length;
+    }
+
+    getOptions() {
+        return (this.#indexTest === -1
+            ? []
+            : this.testArray[this.#indexTest].options);
+    }
+
+    getAnswers() {
+        return this.testArray[this.#indexTest].answer_index;
+    }
+
+    prevQuestion() {
+        --this.#indexTest;
+
+        if (this.#indexTest < 0) {
+            this.#indexTest = 0;
+            return;
+        }
+
+        saveIndexTestToStorage(this.#indexTest);
+        this.onDataChanged();
+    }
+
+    gotoQuestion(num) {
+        console.assert(num > 0 && num < this.getNumQuestions());
+        this.#indexTest = num;
+
+        saveIndexTestToStorage(this.#indexTest);
+        this.onDataChanged();
+    }
+
+    nextQuestion() {
+        ++this.#indexTest;
+
+        const numQuestions = this.getNumQuestions();
+        if (this.#indexTest === numQuestions) {
+            this.#indexTest = numQuestions - 1;
+            alert("Это был последний вопрос.");
+        }
+
+        saveIndexTestToStorage(this.#indexTest);
+        this.onDataChanged();
+    }
+
+    saveAnswer(str) {
+        console.assert(typeof str === 'string');
+
+        this.#arrayOfSelectedAnswers[this.#indexTest] = str;
+        saveAnswersToStorage(this.#arrayOfSelectedAnswers);
+    }
+
+    getModelData() {
+        return {
+            question: this.getQuestion(),
+            options: this.getOptions(),
+            arrayOfSelectedAnswers: this.#arrayOfSelectedAnswers,
+            indexTest: this.#indexTest,
+            numQuestions: this.getNumQuestions()
+        };
+    }
+
+    clearStorage() {
+        window.localStorage.clear();
+    }
+}
+
+class View {
+    constructor()
+    {
+        this.outputText = document.getElementsByTagName('textarea')[0];
+
+        this.form = document.createElement('form');
+
+        this.startButton = document.querySelector('#start');
+        this.gotoButton = document.querySelector('#goto');
+        this.clearButton = document.querySelector('#clear');
+
+        this.backward = document.createElement('button');
+        this.backward.setAttribute('id', 'back');
+        this.backward.innerHTML = 'Назад';
+
+        this.forward = document.createElement('button');
+        this.forward.setAttribute('id', 'forward');
+        this.forward.innerHTML = 'Продолжить';
+
+        this.buttons = document.querySelector('#buttons');
+
+        this.handleAnswer = null;
+    }
+
+    bindClearStorage(handler) {
+        this.clearButton.addEventListener('click', () => {
+            handler();
+        });
+    }
+
+    bindPrevQuestion(handler)
+    {
+        this.backward.addEventListener('click', () => {
+            handler();
+        });
+    }
+
+    bindNextQuestion(handler)
+    {
+        this.forward.addEventListener('click', () => {
+            handler();
+        });
+    }
+
+    bindStartTest(handler)
+    {
+        this.startButton.addEventListener('click', () => {
+            handler();
+        });
+    }
+
+    bindGotoQuestion(handler)
+    {
+        this.gotoButton.addEventListener('click', () => {
+            handler();
+        });
+    }
+
+    bindAnswerTest(handler) {
+        this.handleAnswer = handler;
+    }
+
+    getSelectedAnswers() {
+        let newForm = document.getElementsByTagName('form')[0];
+        let options = newForm.elements;
+
+        let inputs_with_index = Array.prototype.map.call(options, (input, index) => {
+            return {'input': input, 'index': index};
+        });
+
+        let selectedInputs = Array.prototype.filter.call(inputs_with_index, input => {
+            return input['input'].checked;
+        });
+
+        console.assert(selectedInputs.length == 1);
+
+        let tempArray = Array.prototype.map.call(selectedInputs, input => {
+            return input['index'] + 1;
+        });
+
+        console.assert(Array.isArray(tempArray));
+        return tempArray;
+    }
+
+    displayMessage(selectedAnswers, answers) {
+        if (selectedAnswers.length === answers.length && selectedAnswers.toString() === answers.toString()) {
+            this.outputText.value = "Ответ правильный";
+        } else {
+            this.outputText.value = "Ответ неправильный. " + "Правильный ответ: " + answers.toString();
+        }
+    }
+
+    render(modelData) {
+        let {question, options, arrayOfSelectedAnswers, indexTest, numQuestions} = modelData;
+
+        if (indexTest !== -1)
+        {
+            this.buttons.append(this.backward, this.forward);
+            this.startButton.style.display = 'none';
+
+            let testElement = document.getElementById('test');
+            let wrapper = document.getElementById('wrapper');
+
+            removeTextNodes(wrapper);
+            removeTextNodes(testElement);
+
+            {
+                let el = document.getElementById('num_all_questions');
+                el.innerHTML = '/' + numQuestions.toString();
+            }
+
+            this.outputText.value = '';
+
+            this.form.remove();
+
+            let questionNode = document.createElement('p');
+            questionNode.innerHTML = question;
+
+            this.form = document.createElement('form');
+            this.form.appendChild(questionNode);
+
+            {
+                let k = arrayOfSelectedAnswers[indexTest];
+
+                let new_inputs = Array.prototype.map.call(options, (input) => {
+                    let label = document.createElement('label');
+                    label.innerHTML = input;
+
+                    let newInput = document.createElement('input');
+                    newInput.setAttribute('type', 'radio');
+                    newInput.setAttribute('id', 'answer');
+                    newInput.addEventListener('click', this.handleAnswer);
+                    newInput.setAttribute('name', 'input');
+
+                    this.form.append(newInput, label, document.createElement('br'));
+
+                    return newInput;
+                });
+
+                //console.log(indexOfAnswerInTest);
+                //console.log(k);
+
+                if (k != undefined && typeof k == "string")
+                {
+                    let answers = k.split(",");
+
+                    Array.prototype.forEach.call(answers, (answer) => {
+                        new_inputs[+answer - 1].checked = "true";
+                    });
+                }
+            }
+
+            insertAfter(this.form, document.body.firstElementChild.firstElementChild);
+        }
+    }
+}
+
+class Controller {
+    #model;
+    #view;
+
+    constructor(model, view)
+    {
+        this.#model = model;
+        this.#view = view;
+
+        this.#view.bindPrevQuestion(this.prevQuestion);
+        this.#view.bindNextQuestion(this.nextQuestion)
+        this.#view.bindStartTest(this.startTest);
+        this.#view.bindGotoQuestion(this.gotoQuestion);
+        this.#view.bindClearStorage(this.clearStorage);
+
+        this.#view.bindAnswerTest(this.answer);
+
+        this.#model.bindModelDataChanged(this.onDataChanged);
+
+        this.onDataChanged(this.#model.getModelData());
+    }
+
+    onDataChanged = () => {
+        this.#view.render(this.#model.getModelData());
+    }
+
+    nextQuestion = () => {
+        this.#model.nextQuestion();
+    }
+
+    prevQuestion = () => {
+        this.#model.prevQuestion();
+    }
+
+    startTest = () => {
+        this.#model.nextQuestion();
+    }
+
+    gotoQuestion = () => {
+        let questionNumber = Number(document.getElementById('number_question').value);
+
+        if (questionNumber < 1) {
+            alert("Введите положительное число от 1 до " + this.#model.getNumQuestions() + "!");
+            return;
+        }
+
+        this.#model.gotoQuestion(questionNumber - 1);
+    }
+
+    clearStorage = () => {
+        this.#model.clearStorage();
+    }
+
+    answer = () => {
+        let selectedAnswers = this.#view.getSelectedAnswers();
+        this.#model.saveAnswer(selectedAnswers.toString());
+
+        let answers = this.#model.getAnswers();
+        this.#view.displayMessage(selectedAnswers, answers);
+    }
+}
+
+const app = new Controller(new Model(), new View());
+
 function removeTextNodes(node)
 {
     for (var i = node.childNodes.length - 1; i >= 0; --i)
@@ -22,7 +333,7 @@ function removeTextNodes(node)
 
 function loadAnswersFromStorage()
 {
-    if (localStorage.getItem('answer')) {
+    if (localStorage.getItem('answer') !== null) {
 		let arrayOfSelectedAnswers = JSON.parse(localStorage.getItem('answer'));
 	    return arrayOfSelectedAnswers;
 	}
@@ -30,195 +341,23 @@ function loadAnswersFromStorage()
 	return [];
 }
 
+function loadIndexTestFromStorage()
+{
+    if (localStorage.getItem('indexOfAnswerInTest'))
+    {
+        return Number(localStorage.getItem('indexOfAnswerInTest'));
+    }
+
+    return -1;
+}
+
 function saveAnswersToStorage(arrayOfSelectedAnswers)
 {
     localStorage.setItem('answer', JSON.stringify(arrayOfSelectedAnswers));
 }
 
-(function() {
-	var testElement;
+function saveIndexTestToStorage(indexOfAnswerInTest)
+{
+    localStorage.setItem('indexOfAnswerInTest', indexOfAnswerInTest);
+}
 
-	var newForm;
-
-	var indexOfAnswerInTest = 0;
-
-	var arrayOfSelectedAnswers = [];
-
-	window.onload = function() {
-
-		testElement = document.getElementById('test');
-		let wrapper = document.getElementById('wrapper');
-
-		removeTextNodes(wrapper);
-		removeTextNodes(testElement);
-
-	    {
-	        const countQuestions = test_array.length;
-	        let el = document.getElementById('num_all_questions');
-	        el.innerHTML = '/' + countQuestions.toString();
-	    }
-
-		if (localStorage.getItem('indexOfAnswerInTest'))
-        {
-			indexOfAnswerInTest = +localStorage.getItem('indexOfAnswerInTest');
-			moveToNextForm();
-
-			var st = document.getElementById('start');
-			document.getElementById('buttons').style.display = 'block';
-			st.style.display = 'none';
-		}
-	}
-
-    function answer()
-    {
-        var textArea = document.getElementsByTagName('textarea')[0];
-
-        var inputs = newForm.elements;
-
-        let inputs_with_index = Array.prototype.map.call(inputs, (input, index) => {
-            return {'input': input, 'index': index};
-        });
-
-        let selected_inputs = Array.prototype.filter.call(inputs_with_index, (input) => {
-            return input['input'].checked;
-        });
-
-        console.assert(selected_inputs.length == 1);
-
-        let temp_array = Array.prototype.map.call(selected_inputs, (input) => {
-            return input['index'] + 1;
-        });
-
-        //console.log(indexOfAnswerInTest);
-        //console.log(temp_array);
-        //console.log(test_array[indexOfAnswerInTest].answer_index);
-
-        {
-            arrayOfSelectedAnswers[indexOfAnswerInTest] = (temp_array).toString();
-            saveAnswersToStorage(arrayOfSelectedAnswers);
-        }
-
-        let array2 = test_array[indexOfAnswerInTest].answer_index;
-        if (temp_array.length === array2.length && temp_array.every((value, index) => value === array2[index])) {
-            textArea.value = "Ответ правильный";
-        } else {
-            textArea.value = "Ответ неправильный. " + "Правильный ответ: " + (test_array[indexOfAnswerInTest].answer_index).toString();
-        }
-    }
-
-    function moveToNextForm()
-    {
-        localStorage.setItem('indexOfAnswerInTest', indexOfAnswerInTest);
-
-        arrayOfSelectedAnswers = loadAnswersFromStorage();
-
-        newForm = document.createElement('form');
-        var question = document.createElement('p');
-        question.innerHTML = (indexOfAnswerInTest + 1) + ') ' + test_array[indexOfAnswerInTest].question;
-
-        newForm.appendChild(question);
-
-        {
-            const inputs = test_array[indexOfAnswerInTest].options;
-
-            let k = arrayOfSelectedAnswers[indexOfAnswerInTest];
-
-            const len = inputs.length;
-
-            let new_inputs = Array.prototype.map.call(inputs, (input) => {
-                let label = document.createElement('label');
-                label.innerHTML = input;
-
-                let newInput = document.createElement('input');
-                newInput.setAttribute('type', 'radio');
-                newInput.id = "answer";
-                newInput.onclick = answer;
-                newInput.setAttribute('name', 'input');
-
-                newForm.appendChild(newInput);
-                newForm.appendChild(label);
-                newForm.appendChild(document.createElement('br'));
-
-                return newInput;
-            });
-
-            //console.log(indexOfAnswerInTest);
-            //console.log(k);
-
-            if (k != undefined && typeof k == "string")
-            {
-                answers = k.split(",");
-
-                Array.prototype.forEach.call(answers, (answer) => {
-                    new_inputs[+answer - 1].checked = "true";
-                });
-            }
-        }
-
-        insertAfter(newForm, document.body.firstElementChild.firstElementChild);
-    }
-
-    document.getElementById('start').onclick = function() {
-        moveToNextForm();
-        document.getElementById('buttons').style.display = 'block';
-        this.style.display = 'none';
-    }
-
-    document.getElementById('back').onclick = prevQuestion;
-
-    function prevQuestion()
-    {
-        if (indexOfAnswerInTest >= 1)
-        {
-            --indexOfAnswerInTest;
-            var textArea = document.getElementsByTagName('textarea')[0];
-            textArea.value = '';
-
-            newForm.remove();
-            moveToNextForm();
-        }
-    }
-
-    document.getElementById('forward').onclick = nextQuestion;
-
-    document.getElementById('goto').onclick = gotoQuestion;
-
-    function gotoQuestion()
-    {
-        let question_number = document.getElementById('number_question');
-
-        if (+question_number.value < 1)
-        {
-            alert("Введите положительное число от 1 до суммарного количества вопросов.");
-        }
-
-        indexOfAnswerInTest = (+question_number.value - 1);
-        question_number.value = '';
-
-        var textArea = document.getElementsByTagName('textarea')[0];
-        textArea.value = '';
-
-        newForm.remove();
-        moveToNextForm();
-    }
-
-    function nextQuestion()
-    {
-        const countQuestions = testElement.childNodes.length;
-        indexOfAnswerInTest += 1;
-        if (indexOfAnswerInTest != countQuestions) {
-            var textArea = document.getElementsByTagName('textarea')[0];
-            textArea.value = '';
-
-            newForm.remove();
-            moveToNextForm();
-        } else {
-            alert("Это был последний вопрос.");
-        }
-    }
-
-    document.getElementById('clear').onclick = function() {
-        localStorage.clear();
-    }
-
-})();
